ff4e35dfb13b0f1f55df4484b2db0eb685e5deed
Pranav <bizzareforyou@gmail.com>
2026-03-27 14:10:29 +0530

    ai generated dummy data

---
+++ 05_quote_generator/data.js
@@ -0,0 +1,79 @@
+export const quoteData = [
+  {
+    id: 1,
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  },
+  {
+    id: 2,
+    quote: "Innovation distinguishes between a leader and a follower.",
+    author: "Steve Jobs",
+  },
+  {
+    id: 3,
+    quote:
+      "Your time is limited, so don't waste it living someone else's life.",
+    author: "Steve Jobs",
+  },
+  { id: 4, quote: "Stay hungry, stay foolish.", author: "Steve Jobs" },
+  {
+    id: 5,
+    quote:
+      "The future belongs to those who believe in the beauty of their dreams.",
+    author: "Eleanor Roosevelt",
+  },
+  {
+    id: 6,
+    quote: "It does not matter how slowly you go as long as you do not stop.",
+    author: "Confucius",
+  },
+  {
+    id: 7,
+    quote: "Everything you've ever wanted is on the other side of fear.",
+    author: "George Addair",
+  },
+  {
+    id: 8,
+    quote:
+      "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+    author: "Winston Churchill",
+  },
+  {
+    id: 9,
+    quote:
+      "Hardships often prepare ordinary people for an extraordinary destiny.",
+    author: "C.S. Lewis",
+  },
+  {
+    id: 10,
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    id: 11,
+    quote:
+      "The only limit to our realization of tomorrow will be our doubts of today.",
+    author: "Franklin D. Roosevelt",
+  },
+  {
+    id: 12,
+    quote:
+      "Happiness is not something ready made. It comes from your own actions.",
+    author: "Dalai Lama",
+  },
+  {
+    id: 13,
+    quote: "It always seems impossible until it's done.",
+    author: "Nelson Mandela",
+  },
+  {
+    id: 14,
+    quote: "Don't watch the clock; do what it does. Keep going.",
+    author: "Sam Levenson",
+  },
+  {
+    id: 15,
+    quote: "Act as if what you do makes a difference. It does.",
+    author: "William James",
+  },
+];
